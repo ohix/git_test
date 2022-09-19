4a4bf4e6940cb8ab32fa752d49cd3d3aad227cc2
alex <qlaiseq@gmail.com>
2022-09-19 17:14:05 +0200

    Add template for muffin-heyflow

---
+++ work/muffin_heyflow.py
@@ -0,0 +1,18 @@
+titel = "ohren"
+thema = "Ohren"
+
+print(f"ambulante-behandlung-{titel}")
+print(f"ambulante-behandlung-{titel}-details")
+print(f"Ambulante Behandlung - {thema}")
+print(f"Ambulante Behandlung - {thema} - Diagnose")
+print(f"Ambulante Behandlung - {thema} - Beschwerden/Symptome")
+print(f"Ambulante Behandlung - {thema} - erste Diagnose/Beschwerden - Datum")
+print(f"Ambulante Behandlung - {thema} - Diagnose/Beschwerden - Häufigkeit")
+print(f"Ambulante Behandlung - {thema} - Diagnose/Beschwerden - letzte Beschwerde")
+print(f"Ambulante Behandlung - {thema} - Diagnose/Beschwerden - Häufigkeit und Abstände")
+print(f"Ambulante Behandlung - {thema} - Ursachen/Auslöser")
+print(f"Ambulante Behandlung - {thema} - Behandlungen/Untersuchungen/Therapien")
+print(f"Ambulante Behandlung - {thema} - behandlungsfrei")
+print(f"Ambulante Behandlung - {thema} - beschwerdefrei und voll belastbar")
+print(f"Ambulante Behandlung - {thema} - arbeitsunfähig wegen Beschwerden")
+print(f"Ambulante Behandlung - {thema} - Medikamenteneinnahme")
---
+++ work/x.md
@@ -0,0 +1,9 @@
+
+
+... des Rückens oder der Wirbelsäule.
+... der Knochen oder Gelenke.
+... der Haut.
+... durch Allergien.
+... der Verdauung, des Stoffwechsels oder Hämorrhoiden.
+... der Schilddrüse.
+... Hals-Nasen-Ohren.
