7cfce5994a36183aaa89b029f7050293edae6cf5
alex <qlaiseq@gmail.com>
2022-09-30 18:32:44 +0200

    Update muffinXmake automatin

--- work/muffin_makedotcom.py
+++ work/muffin_makedotcom.py
@@ -1,7 +1,11 @@
-
-for i in range ( 1 , 44 ):
-    print(i)
-    print("{{1.fields["+str(i)+"].label}}")
-    print("{{1.fields["+str(i)+"].values[1].answer}}")
-    print("---")
-
+with open('/Users/alex/Documents/the_odin_project/git_test/work/x.txt', 'w') as f:
+    for i in range ( 1 , 301 ):
+        f.write(str(i) + " {{1.fields["+str(i)+"].label}}")
+        f.write('\n')
+        for j in range (1,1):
+            f.write("{{1.fields["+str(i)+"].values["+str(j)+"].answer}}")
+            f.write('\n')
+        f.write("---")
+        f.write('\n')
+print("Done")
+f.close()
